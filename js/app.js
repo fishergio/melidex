5cb786d51c6ceb254dec493a1746dfb52cdcc26c
fishergio <sergio@tingsystems.com>
2017-01-04 23:35:56 -0600

    attach comments to controller and add ng-change

--- js/app.js
+++ js/app.js
@@ -35,11 +35,18 @@
 
     app.controller('CommentsController', function () {
         this.comments = [];
+        this.comment = {};
         this.show = false;
 
         this.toggle = function () {
             this.show = !this.show;
         };
+
+        this.anonymousChanged = function () {
+            if(this.comment.anonymous){
+                this.comment.email = "";
+            }
+        }
     });
 
     app.filter('imageify', function () {
