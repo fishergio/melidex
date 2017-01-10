ee92c43fbfa7cb8687faa96eeaa9638c310f355e
fishergio <sergio@tingsystems.com>
2017-01-10 01:06:42 -0600

    create a module for controllers

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@
  * Created by sergio on 26/12/16.
  */
 (function () {
-    var app = angular.module('pokedex',[]);
-
-
+    var app = angular.module('pokedex',[
+        'pokedex.controllers'
+    ]);
 
     app.controller('CommentsController', function () {
 
