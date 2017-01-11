24a9e892f45260b1daa2a0f39b1e90fc8ea1f479
fishergio <sergio@tingsystems.com>
2017-01-11 01:07:35 -0600

    create view for pokemon and add ngRoute

--- js/app.js
+++ js/app.js
@@ -3,8 +3,29 @@
  */
 (function () {
     var app = angular.module('pokedex',[
+        'ngRoute',
         'pokedex.controllers',
         'pokedex.directives',
         'pokedex.filters'
     ]);
+
+    app.config(['$routeProvider', function ($routeProvider) {
+
+        $routeProvider
+            .when('/',{
+                templateUrl: 'views/pokedex.html'
+            })
+            .when('/pokemon/:id',{
+              templateUrl: 'views/pokemon.html',
+              controller: 'PokemonController',
+              controllerAs: 'cmtsCtrl'
+            })
+            .otherwise({
+               redirectTo: '/'
+            });
+    }]);
+
+    /*app.config(function($routeProvider){
+    });*/
+
 })();
